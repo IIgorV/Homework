8a4954595b1671677286627a9032d9596a931a25
IIgorV <igorek.isakov86@gmail.com>
2024-09-25 18:10:17 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "igor"
+print('Name:', name[0].upper() + name[1:], type(name))
+Age = 33
+print('Age:', Age, type(Age))
+print('New Age:',Age+1)
+Igor = 'Student'
+print('Is Student:', Igor == 'Student')
+
+
